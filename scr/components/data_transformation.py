eb39214032203a7a70d1ea98853b54fc9cfb94d3
Vishal00812 <vktawesome568@gmail.com>
2024-05-02 10:32:44 +0530

    prediction is done here

--- scr/components/data_transformation.py
+++ scr/components/data_transformation.py
@@ -7,30 +7,29 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from category_encoders.target_encoder import TargetEncoder
 
 from scr.exception import CustomException
 from scr.logger import logging
 import os
 
 from scr.utils import save_object
-
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function is responsible for data transformation
-        
-        '''
+ #Delivery_person_Age,Delivery_person_Ratings,Restaurant_latitude,Restaurant_longitude,Delivery_location_latitude,
+ #Delivery_location_longitude,Order_Date,Time_Orderd,Time_Order_picked,Weather_conditions
+ #,Road_traffic_density,Vehicle_condition,Type_of_order,Type_of_vehicle,multiple_deliveries,Festival,City,Time_taken (min)      
         try:
             numerical_columns = ['Delivery_person_Age','Delivery_person_Ratings','multiple_deliveries','Vehicle_condition']
 
-            categorical_columns_with_less_classes=['Weather_conditions','Road_traffic_density',
+            categorical_columns_with_less_classes=['Weather_condition','Road_traffic_density',
                                        'Type_of_order','Type_of_vehicle','Festival','City']
 
             categorical_columns_with_multiple_classes=['Order_Date','Time_Orderd','Time_Order_picked']
@@ -58,6 +57,7 @@ class DataTransformation:
 
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("TargetEncoder",TargetEncoder()),
                 ("scaler",StandardScaler(with_mean=False))
                 ]
 
@@ -70,9 +70,6 @@ class DataTransformation:
             preprocessor=ColumnTransformer(
                 [
                 ("num_pipeline",num_pipeline,numerical_columns),
-                
-                    
-                
                 ("cat_pipeline_with_less_classes",cat_pipeline_with_less_classes,categorical_columns_with_less_classes),
                 ("cat_pipeline_with_multiple_classes",cat_pipeline_with_multiple_classes,categorical_columns_with_multiple_classes)
                     
@@ -81,12 +78,9 @@ class DataTransformation:
 
             return preprocessor
         
+        
         except Exception as e:
             raise CustomException(e,sys)
-        
-
-
-
         
     def initiate_data_transformation(self,train_path,test_path):
 
@@ -94,15 +88,14 @@ class DataTransformation:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
 
-            logging.info("Reading of train and test data completed")
+            logging.info("Read train and test data completed")
 
             logging.info("Obtaining preprocessing object")
 
             preprocessing_obj=self.get_data_transformer_object()
 
             target_column_name="Time_taken (min)"
-            numerical_columns = ['Delivery_person_Age','Delivery_person_Ratings','multiple_deliveries','Vehicle_condition']
-
+            numerical_columns =  ['Delivery_person_Age','Delivery_person_Ratings','multiple_deliveries','Vehicle_condition']
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
---
+++ scr/pipeline/predict_pipeline.py
@@ -0,0 +1,107 @@
+import sys
+import pandas as pd
+from scr.exception import CustomException
+from scr.logger import logging
+from scr.utils import load_object
+
+from sklearn.preprocessing import StandardScaler
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path='artifacts\model.pkl'
+            preprocessor_path='artifacts\proprocessor.pkl'
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            logging.info('Exception occured in prediction pipeline')
+            raise CustomException(e,sys)
+
+
+
+class CustomData:
+    def __init__(  self,
+
+
+        Delivery_person_Age: float,
+        Delivery_person_Ratings: float,
+        Restaurant_latitude:float,
+        Restaurant_longitude:float,
+        Delivery_location_latitude:float,
+        Delivery_location_longitude:float,
+        Order_Date:str,
+        Time_Orderd:str,
+        Time_Order_picked: str,
+        Weather_condition: str,
+        Road_traffic_density: str,
+        Vehicle_condition: int,
+        Type_of_order: str,
+        Type_of_vehicle: str,
+        multiple_deliveries: int,
+        Festival: str,
+        City: str
+        
+        
+        ):
+
+        self.Delivery_person_Age = Delivery_person_Age
+
+        self.Delivery_person_Ratings = Delivery_person_Ratings
+
+        self.Restaurant_latitude = Restaurant_latitude
+
+        self.Restaurant_longitude = Restaurant_longitude
+
+        self.Delivery_location_latitude = Delivery_location_latitude
+        self.Delivery_location_longitude = Delivery_location_longitude
+
+        self.Order_Date = Order_Date
+
+        self.Time_Orderd = Time_Orderd
+        self.Time_Order_picked = Time_Order_picked
+        self.Weather_condition = Weather_condition
+        self.Road_traffic_density = Road_traffic_density
+        self.Vehicle_condition = Vehicle_condition
+        self.Type_of_order = Type_of_order
+        self.Type_of_vehicle = Type_of_vehicle
+        self.multiple_deliveries = multiple_deliveries
+        self.Festival = Festival
+        self.City = City
+
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "Delivery_person_Age": [self.Delivery_person_Age],
+                "Delivery_person_Ratings": [self.Delivery_person_Ratings],
+                "Restaurant_latitude": [self.Restaurant_latitude],
+                "Restaurant_longitude": [self.Restaurant_longitude],
+                "Delivery_location_latitude": [self.Delivery_location_latitude],
+                "Delivery_location_longitude": [self.Delivery_location_longitude],
+                "Order_Date": [self.Order_Date],
+                "Time_Orderd": [self.Time_Orderd],
+                "Time_Order_picked": [self.Time_Order_picked],
+                "Weather_condition": [self.Weather_condition],
+                "Road_traffic_density": [self.Road_traffic_density],
+                "Vehicle_condition": [self.Vehicle_condition],
+                "Type_of_order": [self.Type_of_order],
+                "Type_of_vehicle": [self.Type_of_vehicle],
+                "multiple_deliveries": [self.multiple_deliveries],
+                "Festival": [self.Festival],
+                "City": [self.City],
+         
+            }
+            logging.info('Dataframe Gathered')
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e, sys)
